e16657c9dc73b63d29407bdf0d6103d32ea4cf25
nyingi <nyingimaina@gmail.com>
2020-02-19 18:07:27 +0300

    Add project files.

---
+++ Rocket.Libraries.TaskRunnerTests/Conditions/PreconditionEvaluatorTests.cs
@@ -0,0 +1,38 @@
+﻿using Rocket.Libraries.TaskRunner.Conditions;
+using Rocket.Libraries.TaskRunner.TaskDefinitions;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rocket.Libraries.TaskRunnerTests.Conditions
+{
+    public class PreconditionEvaluatorTests
+    {
+        [Theory]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        public async Task PreConditionsFilterCorrectly(bool preconditionResult, bool errorMessageShouldBeEmpty)
+        {
+            var taskDefinition = new TaskDefinition<Guid>
+            {
+                Id = Guid.NewGuid(),
+                Name = "Some Name"
+            };
+
+            var preconditions = ImmutableList<TaskPrecondition<Guid>>.Empty.Add(new TaskPrecondition<Guid>
+            {
+                Condition = async (a) => await Task.Run(() => preconditionResult),
+                DisplayLabel = "Blah",
+                TaskName = taskDefinition.Name
+            });
+
+            var preconditionEvaluator = new PreconditionEvaluator<Guid>();
+            var result = await preconditionEvaluator.GetFailingPrecondition(taskDefinition, preconditions);
+            var errorMessageIsEmpty = string.IsNullOrEmpty(result);
+            Assert.Equal(errorMessageShouldBeEmpty, errorMessageIsEmpty);
+        }
+    }
+}
---
+++ Rocket.Libraries.TaskRunnerTests/Runner/RunManagerTests.cs
@@ -0,0 +1,64 @@
+﻿using Moq;
+using Rocket.Libraries.TaskRunner.Conditions;
+using Rocket.Libraries.TaskRunner.Histories;
+using Rocket.Libraries.TaskRunner.Runner;
+using Rocket.Libraries.TaskRunner.Schedules;
+using Rocket.Libraries.TaskRunner.TaskDefinitions;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rocket.Libraries.TaskRunnerTests.Runner
+{
+    public class RunManagerTests
+    {
+        [Fact]
+        public async Task TaskIsRunSuccessfully()
+        {
+            var taskDefinitionId = Guid.NewGuid();
+            var scheduleReader = new Mock<IScheduleReader<Guid>>();
+            var taskDefinitionReader = new Mock<ITaskDefinitionReader<Guid>>();
+            var runner = new Mock<IRunner<Guid>>();
+            var historyWriter = new Mock<IHistoryWriter<Guid>>();
+            var scheduleWriter = new Mock<IScheduleWriter<Guid>>();
+            var preconditionReader = new Mock<IPreconditionReader<Guid>>();
+
+            scheduleReader.Setup(a => a.GetAllAsync())
+                .ReturnsAsync(ImmutableList<Schedule<Guid>>.Empty.Add(new Schedule<Guid>
+                {
+                    LastRun = DateTime.Now.AddDays(-1),
+                    TaskDefinitionId = taskDefinitionId,
+                }));
+
+            taskDefinitionReader.Setup(a => a.GetByIdsAsync(It.IsAny<ImmutableList<Guid>>()))
+                .ReturnsAsync(ImmutableList<TaskDefinition<Guid>>.Empty.Add(new TaskDefinition<Guid>
+                {
+                    Id = taskDefinitionId
+                }));
+
+            runner.Setup(a => a.RunAsync(It.IsAny<TaskDefinition<Guid>>()))
+                .ReturnsAsync(new SingleTaskRunResult
+                {
+                    Remarks = "Blah",
+                    Succeeded = true
+                });
+
+            var runManager = new RunManager<Guid>(
+                scheduleReader.Object,
+                taskDefinitionReader.Object,
+                runner.Object,
+                historyWriter.Object,
+                scheduleWriter.Object,
+                preconditionReader.Object
+                );
+
+            var result = await runManager.RunAsync();
+            Assert.True(result.Histories != null);
+            Assert.True(result.Histories.First().Status == RunHistoryStatuses.CompletedSuccessfully);
+        }
+    }
+}
